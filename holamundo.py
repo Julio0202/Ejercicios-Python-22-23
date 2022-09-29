b44834c6381fd10a3102987b0ff517cf597975c0
Julio0202 <al_ychen@iessierradelasvillas.org>
2022-09-29 10:02:56 +0200

    Comentarios, entradas y salidas de datos y tipos de datos en python

--- holamundo.py
+++ holamundo.py
@@ -1,5 +1,21 @@
 from asyncore import read
+from errno import EADDRNOTAVAIL
+from sre_constants import JUMP
 import string
 
+#Tipos de datos y variables
+ #Numericos
+  #enteros - - int
+  #real - - float
+ #Cadenas de caracteres - - str
+ #Lógico - - bool
+mayor_menorEdad=False
+edad = 0
+nombre = ""
 
-print ("Hola mundo")
+#Entrada y salida de datos 
+#Salida con print
+#Entrada de datos - - input
+nombre = input("Dime tu nombre:\n")
+edad = input("Dime tu edad:\n")
+print ("Buenos días", nombre, "tu edad es", edad)
