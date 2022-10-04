0deb1a3df7940d1397f838e4f09653ce2f4209bd
Julio0202 <al_ychen@iessierradelasvillas.org>
2022-10-04 14:18:49 +0200

    Proyecto Spider-Man

---
+++ RetoElHombreAraña.py
@@ -0,0 +1,12 @@
+#Programa El hombre araña también conocido como Spider-man.
+
+print("Dime 1era distancia y 2nd distancia respectivamente")
+dis1 = int(input())
+dis2 = int(input())
+if abs(dis1)>abs(dis2):
+    print("Tendrás que ir al segundo camino")
+else:
+    print("Tendrás que ir al primer camino")
+print("La distancia recorrida sería", ((abs(dis1))*2)+abs(dis2))
+
+
