c6b71522fac9e3b3d407a0ebd9dc39a00be097e0
Julio Chen <al_ychen@iessierradelasvillas.org>
2022-10-17 11:15:03 +0200

    Agenda Telefono

--- AgendaTelefono.py
+++ AgendaTelefono.py
@@ -1,16 +1,23 @@
-'''
-Opción 1:
-Lista para nombres
-Lista para telefonos
 
-Opción 2:
-Lista para nombres y telefonos 
-Ejemplo [Juan - Telefono, Pepe - Telefono]
-
+#Creamos el menu de la agenda
 '''
-#Opción 1
-vNomb = []
-vTelf = []
-vNomb.append(input("Dime un nombre: "))
-vTelf.append(input("Dime un telefono: "))
-print("El telefono de", vNomb.pop(),"es",vTelf.pop())
+1- Insertar Conctacto
+2- Borrar contacto
+3- Buscar contacto
+4- Ver todos los contactos 
+5- Salir
+'''
+comodin = 0
+print("1-Insertar Contacto\n 2-Borrar contacto\n 3-Buscar contacto\n 4-Ver todos los contactos\n 5-Salir")
+while (comodin!=5):
+    vnomb = []
+    vtelef = []
+    print("Inserta el número")
+    num = int(input())
+    if num==1:
+        print("Dime su contacto")
+        contact = str(input())
+        vnomb.append(contact)
+        print("Dime su numero")
+        telef = int(input())
+        vtelef.append(telef)
