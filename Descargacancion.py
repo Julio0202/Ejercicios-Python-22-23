e541971dcee4ed291589d4ebf701aa06584c61e0
Julio Chen <al_ychen@iessierradelasvillas.org>
2022-10-27 09:59:46 +0200

    Descarga canciones y listas

--- Descargacancion.py
+++ Descargacancion.py
@@ -1,8 +1,17 @@
 from pytube import YouTube  
+from pytube import Playlist
 def descargaCancion(url:str):
-    youtube = Youtube(url)
+    youtube = YouTube(url)
     print(youtube.author)
     print("Descargando", youtube.title)
     cancion = youtube.streams.get_audio_only()
     cancion.download
-descargaCancion("")
+def descargarLista(url:str):
+    playlist = Playlist(url)
+    for cancion in playlist.videos:
+        cancion.streams.get_audio_only().download("Canciones")
+        print("Descargando cancion:", cancion.author)
+        
+
+url = "https://www.youtube.com/watch?v=HKNz0DWQXZw&list=PLVwXkzK42QWrWyeunQ_hi4OM8iUrBWUlq"
+descargarLista(url)
---
+++ Setenciasrepetitivas.py
@@ -0,0 +1,11 @@
+#While 
+contador=0
+while contador<=10:
+    print(contador)
+    contador+=1
+#Bucle for
+#Lista [1,2,3,4,5,6,7,8,9,10]
+vnomb = ["Angel","Maria","Pepe","Jose"]
+for num in vnomb:   
+    print(num)      
+    print("HEllo")
