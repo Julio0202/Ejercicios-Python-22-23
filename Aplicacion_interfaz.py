e36ad7132f568403c3aabbcce67769b85acdd45f
Julio Chen <al_ychen@iessierradelasvillas.org>
2022-11-25 11:15:47 +0100

    INICIO SESIÓN

--- Aplicacion_interfaz.py
+++ Aplicacion_interfaz.py
@@ -1,5 +1,27 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+vlistas = []
+usuario = ""
+contrasena = ""
+confirmarcontra = ""
+def guardarDatos():
+    usuario = entry_nombre_usuario.get()
+    contrasena = entry_pass_usuario.get()
+    confirmarcontra = entry_repite_pass_usuario.get()
+    if contrasena == confirmarcontra:
+        vlistas.append(usuario)
+        vlistas.append(contrasena)
+        entry_nombre_usuario.delete(0,len(usuario))
+        entry_pass_usuario.delete(0,len(contrasena))
+        entry_repite_pass_usuario.delete(0,len(confirmarcontra)) 
+        messagebox.showinfo("Usuario Guardado", f"Usuario {usuario} guardado")
+def listasDatos():
+    print(vlistas)
+
+
+
+
 
 ventana = Tk()
 ventana.title("Iniciar sesión")
@@ -13,27 +35,27 @@ entry_nombre_usuario = ttk.Entry(ventana)
 label_nombre_usuario = ttk.Label(ventana,text="Nombre usuario:")
 
 label_pass_usuario = ttk.Label(ventana,text="Contraseña:")
-entry_pass_usuario = ttk.Entry(ventana)
+entry_pass_usuario = ttk.Entry(ventana,show="*")
 
 label_repite_pass_usuario = ttk.Label(ventana,text="Confirma contraseña")
-entry_repite_pass_usuario = ttk.Entry(ventana)
+entry_repite_pass_usuario = ttk.Entry(ventana,show="*")
 
-boton_guardar = ttk.Button(ventana,text="Guardar")
-boton_salir = ttk.Button(ventana,text="Salir")
+boton_guardar = ttk.Button(ventana,text="Guardar",command=guardarDatos)
+boton_salir = ttk.Button(ventana,text="Salir",command=ventana.destroy)
 
 #Pintar en pantalla los componentes
 label_titulo.grid(row=0,column=1)
-label_nombre_usuario.grid(row=1, column=0)
-entry_nombre_usuario.grid(row=1, column=1)
+label_nombre_usuario.grid(row=1, column=0, pady=10)
+entry_nombre_usuario.grid(row=1, column=1, pady=10)
 
-label_pass_usuario.grid(row=2,column=0)
-entry_pass_usuario.grid(row=2,column=1)
+label_pass_usuario.grid(row=2,column=0, pady=10)
+entry_pass_usuario.grid(row=2,column=1, pady=10)
 
-label_repite_pass_usuario.grid(row=3,column=0)
-entry_repite_pass_usuario.grid(row=3,column=1)
+label_repite_pass_usuario.grid(row=3,column=0, pady=10, padx=15)
+entry_repite_pass_usuario.grid(row=3,column=1, pady=10)
 
-boton_guardar.grid(row=5,column=0)
-boton_salir.grid(row=5,column=1)
+boton_guardar.grid(row=5,column=0, pady=10)
+boton_salir.grid(row=5,column=1, pady=10)
 
 
 
