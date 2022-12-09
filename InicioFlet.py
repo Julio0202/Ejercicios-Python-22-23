ac0f35c5ac5cc1a999bff7f1937d40ccd9d2040e
Julio Chen <al_ychen@iessierradelasvillas.org>
2022-12-09 10:43:21 +0100

    Flet

--- InicioFlet.py
+++ InicioFlet.py
@@ -2,18 +2,24 @@ import flet as f
 
 
 def main(page: f.Page):
-    def cambiarColor(e):
-        for i in range(11):
-            text = f.Text(value=f"Texto numero {i}",size=30)
-            page.add(text)
-
+    page.title="Flet inicio"
+    def cambiar_color(e):
+        t.value = textField_Nombre.value 
+        page.update()
     #Componente texto 
     t = f.Text(value="Introducción a Flet", color="blue",size=66)
     page.add(t) #Add hace dos cosas 1- Añadir 2- Actualizar
     t.value="Cambiando los datos"
+    page.update()
     #Actualiza los datos: page.update()
     #Componente boton
-    bt = f.FloatingActionButton(icon=f.icons.ADD, on_click=cambiarColor)
+    bt = f.FloatingActionButton(icon=f.icons.ADD, on_click=cambiar_color)
     page.add(bt)
+    textField_Nombre = f.TextField(label="Nombre",hint_text="Escribe tu nombre")
+    page.add(textField_Nombre)
+    dropDown_Menu = f.Dropdown(width=300,options=[f.dropdown.Option("L")])
+    page.add(dropDown_Menu)
+    dropDown_Menu.options.append(f.dropdown.Option("Nueva"))
+    page.update()
 
 f.app(target=main)
